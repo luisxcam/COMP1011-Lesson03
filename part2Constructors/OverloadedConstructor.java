54f2ece18295f3215e582b8a8a7636753d8e9e81
luisxcam <luisxcam@gmail.com>
2014-09-17 13:46:03 -0400

    Added Constructors to the java class

--- part2Constructors/OverloadedConstructor.java
+++ part2Constructors/OverloadedConstructor.java
@@ -1,4 +1,22 @@
-
 public class OverloadedConstructor {
+	//variable definition
+	private int iv1;
+	private float iv2;
+	private String iv3;
 
+	public OverloadedConstructor(){
+
+	}
+
+	public OverloadedConstructor(int iv1){
+
+	}
+
+	public OverloadedConstructor(float iv2){
+
+	}
+	
+	public OverloadedConstructor(String iv3){
+		
+	}
 }
